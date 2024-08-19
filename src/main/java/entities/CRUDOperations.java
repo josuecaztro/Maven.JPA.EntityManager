14a48312c191f0aeca4f118cef73e2f7ead4801b
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-19 11:29:50 -0400

    one more method left

--- src/main/java/entities/CRUDOperations.java
+++ src/main/java/entities/CRUDOperations.java
@@ -3,6 +3,9 @@ package entities;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class CRUDOperations {
 
@@ -16,16 +19,49 @@ public class CRUDOperations {
         System.out.println("Entity added!");
     }
 
-    public void findById(int id){
+    public CD findById(int id){
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         EntityTransaction et = em.getTransaction();
         et.begin();
         CD aCd = em.find(CD.class, id);
+        System.out.println("===== ENTITY FOUND! =====");
         System.out.println("Title: " + aCd.getTitle());
         System.out.println("Description: " + aCd.getDescription());
         System.out.println("Year: " + aCd.getYear());
         System.out.println("Artist: " + aCd.getArtist());
         System.out.println("Price: " + aCd.getPrice());
+        em.getTransaction().commit();
+        em.close();
+        return aCd;
+    }
+
+    public void findAll(){
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+
+        TypedQuery<CD> query = em.createQuery("SELECT c FROM CD c", CD.class);
+        List<CD> cds = query.getResultList();
+        for (CD x : cds){
+            System.out.println("===== ID #: " + x.getId() + " =====");
+            System.out.println("Title: " + x.getTitle());
+            System.out.println("Description: " + x.getDescription());
+            System.out.println("Year: " + x.getYear());
+            System.out.println("Artist: " + x.getArtist());
+            System.out.println("Price: " + x.getPrice());
+            System.out.println();
+        }
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void removeEntity(int id){
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+
+        CD aCd = em.find(CD.class, id);
+        em.remove(aCd);
+        System.out.println("===== " + aCd.getTitle() + " has been removed. =====" );
         em.getTransaction().commit();
         em.close();
     }
--- src/main/java/entities/Main.java
+++ src/main/java/entities/Main.java
@@ -5,12 +5,8 @@ public class Main {
     public static void main(String[] args) {
         CRUDOperations co = new CRUDOperations();
 
+        co.findAll();
 
-//        CD cd = new CD(5, "TestCD", "This is a test description", 1999, "Bob Smith", 24.99);
-//        co.insertEntity(cd);
-
-
-//        co.findById(3);
     }
 
 }
